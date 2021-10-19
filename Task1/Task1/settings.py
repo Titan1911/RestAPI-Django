9b1fce0e2131bb5bc06a233df1cea26d82c67fb7
Sahil Ahuja <sahilahuja1911@gmail.com>
2021-10-19 22:49:41 +0530

    "some modifications"

--- Task1/Task1/settings.py
+++ Task1/Task1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dq#xth9(jg+l!5y0$u+qt&jgh^0)wo_yaizxau5k&uvehlhfuy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
